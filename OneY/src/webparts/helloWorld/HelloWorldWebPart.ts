1e4c87eca5d018459f7d700580708a04a0d93901
Deepak Rajput <PO30201@posten.local>
2018-09-28 15:05:52 +0200

    Demo 1820

--- OneY/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ OneY/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -21,8 +21,8 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
         <div class="${ styles.container }">
           <div class="${ styles.row }">
             <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.DEMOTEST</p>
+              <span class="${ styles.title }">Welcome to Today's Demo</span>
+              <p class="${ styles.subTitle }"><H1>Hello EveryOne</H1></p>
               <p class="${ styles.description }">${escape(this.properties.description)}</p>
               <a href="https://aka.ms/spfx" class="${ styles.button }">
                 <span class="${ styles.label }">Learn more</span>
